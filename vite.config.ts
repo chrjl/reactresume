c9e1de39c6cedca5e9a28f67a86d69f90b06afb3
chrjl <73675826+chrjl@users.noreply.github.com>
2024-11-01 16:37:04 -0700

    Squashed 'packages/components/' content from commit 929a2cb
    git-subtree-dir: packages/components
    git-subtree-split: 929a2cb86aafef000deebbdb80a71aa82e0c4c98

---
+++ vite.config.ts
@@ -0,0 +1,34 @@
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import dts from 'vite-plugin-dts';
+import { libInjectCss } from 'vite-plugin-lib-inject-css';
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'lib/main.ts'),
+      formats: ['es'],
+      name: '@reactresume/components',
+      fileName: 'components',
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+  },
+  plugins: [
+    react(),
+    dts({
+      include: ['lib'],
+      tsconfigPath: './tsconfig.app.json',
+      insertTypesEntry: true,
+    }),
+    libInjectCss(),
+  ],
+});
