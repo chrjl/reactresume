3b16793b1579af1fd22545b68faacfbf8e04b424
chrjl <73675826+chrjl@users.noreply.github.com>
2024-11-02 08:49:50 -0700

    feat: self-host fonts

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,12 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import '@fontsource-variable/inter/wght.css';
+import '@fontsource-variable/inter/wght-italic.css';
+import '@fontsource-variable/arimo/wght.css';
+import '@fontsource-variable/arimo/wght-italic.css';
+import '@fontsource/cousine/400.css';
+import '@fontsource/cousine/700.css';
 
 import './reset.css';
 import './index.css';
