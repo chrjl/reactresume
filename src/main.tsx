e0df1578a78d76eb9d2147740df47311351be6d3
chrjl <73675826+chrjl@users.noreply.github.com>
2024-11-09 12:44:11 -0800

    build: remove fontsource fonts

--- src/main.tsx
+++ src/main.tsx
@@ -2,17 +2,6 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
-import '@fontsource-variable/inter/wght.css';
-import '@fontsource-variable/inter/wght-italic.css';
-import '@fontsource-variable/arimo/wght.css';
-import '@fontsource-variable/arimo/wght-italic.css';
-import '@fontsource/cousine/400.css';
-import '@fontsource/cousine/700.css';
-
 import './reset.css';
 import './index.css';
 
